a75d3d2dccb17f3756b83ba053cb1bc3d4f420eb
gulmina <gulmina44@gmail.com>
2024-05-26 11:52:58 +0200

    spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import CartItem from "./components/CartItem";
 import HouseCart from "./components/HouseCart";
 import "./components/style.css";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
-  return (
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return loading ? (
+    <ClipLoader color={"green"} loading={loading} size={100} />
+  ) : (
     <div className="app-container">
-  
       <HouseCart />
 
       <CartItem />
